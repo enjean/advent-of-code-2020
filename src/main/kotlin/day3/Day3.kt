994a14235afa27b1d0db4eb7bc5e481869af69d5
Eric Jean <eric.jean@wwt.com>
2020-12-03 16:52:27 -0700

    Day 3 Part 2

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -2,20 +2,22 @@ package day3
 
 import java.io.File
 
-fun traverseForest(forestLines: List<String>) : Int {
+fun traverseForest(forestLines: List<String>, right: Int, down: Int) : Int {
     val forestModel = forestLines.map { line ->
         line.map {
             it == '#'
         }
     }
 
+    var currentRow = 0
     var currentColumn = 0
     var treesHit = 0
-    for(row in forestModel.indices) {
-        if (forestModel[row][currentColumn % forestModel[row].size]) {
+    while(currentRow < forestModel.size) {
+        if (forestModel[currentRow][currentColumn % forestModel[currentRow].size]) {
             treesHit++
         }
-        currentColumn += 3
+        currentRow += down
+        currentColumn += right
     }
 
     return treesHit
@@ -24,5 +26,12 @@ fun traverseForest(forestLines: List<String>) : Int {
 fun main()  {
     val forestLines = File("src/main/resources/day3/input.txt").readLines()
 
-    println("Part 1 = ${traverseForest(forestLines)}")
+    val right3Down1 = traverseForest(forestLines, 3, 1)
+    println("Part 1 = $right3Down1")
+
+    val right1Down1 = traverseForest(forestLines, 1, 1)
+    val right5Down1 = traverseForest(forestLines, 5, 1)
+    val right7Down1 = traverseForest(forestLines, 7, 1)
+    val right1Down2 = traverseForest(forestLines, 1, 2)
+    println("Part 2 = ${right1Down1 * right3Down1 * right5Down1 * right7Down1 * right1Down2}")
 }
--- src/test/kotlin/day3/Day3Test.kt
+++ src/test/kotlin/day3/Day3Test.kt
@@ -7,22 +7,43 @@ import org.junit.jupiter.api.Test
 class Day3Test {
     @Nested
     inner class Part1 {
+        val forestLines = listOf(
+            "..##.......",
+            "#...#...#..",
+            ".#....#..#.",
+            "..#.#...#.#",
+            ".#...##..#.",
+            "..#.##.....",
+            ".#.#.#....#",
+            ".#........#",
+            "#.##...#...",
+            "#...##....#",
+            ".#..#...#.#"
+        )
+
         @Test
-        fun `Part 1 Example`() {
-            val forestLines = listOf(
-                "..##.......",
-                "#...#...#..",
-                ".#....#..#.",
-                "..#.#...#.#",
-                ".#...##..#.",
-                "..#.##.....",
-                ".#.#.#....#",
-                ".#........#",
-                "#.##...#...",
-                "#...##....#",
-                ".#..#...#.#"
-            )
-            assertEquals(7, traverseForest(forestLines))
+        fun `Right 1, down 1`() {
+            assertEquals(2, traverseForest(forestLines, 1, 1))
+        }
+
+        @Test
+        fun `Right 3, down 1`() {
+            assertEquals(7, traverseForest(forestLines, 3, 1))
+        }
+
+        @Test
+        fun `Right 5, down 1`() {
+            assertEquals(3, traverseForest(forestLines, 5, 1))
+        }
+
+        @Test
+        fun `Right 7, down 1`() {
+            assertEquals(4, traverseForest(forestLines, 7, 1))
+        }
+
+        @Test
+        fun `Right 1, down 2`() {
+            assertEquals(2, traverseForest(forestLines, 1, 2))
         }
     }
 }
