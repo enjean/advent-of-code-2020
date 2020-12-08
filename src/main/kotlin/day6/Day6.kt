06541c069f124c6e7edabfb8bbb13d9e46a6a90e
Eric Jean <eric.jean@wwt.com>
2020-12-07 23:10:05 -0700

    Day 6 Part 2

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -3,18 +3,31 @@ package day6
 import util.ParseUtil
 import java.io.File
 
-fun uniqueQuestions(groupAnswerLines: List<String>) : Int =
+fun uniqueQuestions(groupAnswerLines: List<String>): Int =
     groupAnswerLines.fold(emptySet<Char>()) { questions, line ->
         questions.plus(line.toSet())
     }.size
 
-fun totalUniqueQuestions(lines: List<String>): Int =
-    ParseUtil.parseGroups(lines)
+fun allAnsweredYes(groupAnswerLines: List<String>): Int =
+    groupAnswerLines.first()
+        .count { questionChar ->
+            groupAnswerLines.all { answerLine -> answerLine.contains(questionChar) }
+        }
+
+
+fun totalUniqueQuestions(groupAnswers: List<List<String>>): Int =
+    groupAnswers
         .map { uniqueQuestions(it) }
         .sum()
 
-fun main() {
-    val lines = File("src/main/resources/day6/input.txt").readLines()
+fun totalAllAnswered(groupAnswers: List<List<String>>): Int =
+    groupAnswers
+        .map { allAnsweredYes(it) }
+        .sum()
 
-    println("Part 1 = ${totalUniqueQuestions(lines)}")
+fun main() {
+    val groups = ParseUtil.parseGroups(File("src/main/resources/day6/input.txt").readLines())
+
+    println("Part 1 = ${totalUniqueQuestions(groups)}")
+    println("Part 2 = ${totalAllAnswered(groups)}")
 }
--- src/test/kotlin/day6/Day6KtTest.kt
+++ src/test/kotlin/day6/Day6KtTest.kt
@@ -1,102 +1,208 @@
 package day6
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import util.ParseUtil
 
 class Day6KtTest {
-    @Test
-    fun `Example 1`() {
-        assertEquals(
-            6, uniqueQuestions(
-                listOf(
-                    "abcx",
-                    "abcy",
-                    "abcz"
+    @Nested
+    inner class UniqueQuestions {
+        @Test
+        fun `Example 1`() {
+            assertEquals(
+                6, uniqueQuestions(
+                    listOf(
+                        "abcx",
+                        "abcy",
+                        "abcz"
+                    )
                 )
             )
-        )
-    }
+        }
 
-    @Test
-    fun `Example 2`() {
-        assertEquals(
-            3, uniqueQuestions(
-                listOf(
-                    "abc"
+        @Test
+        fun `Example 2`() {
+            assertEquals(
+                3, uniqueQuestions(
+                    listOf(
+                        "abc"
+                    )
                 )
             )
-        )
-    }
+        }
 
-    @Test
-    fun `Example 3`() {
-        assertEquals(
-            3, uniqueQuestions(
-                listOf(
-                    "a",
-                    "b",
-                    "c"
+        @Test
+        fun `Example 3`() {
+            assertEquals(
+                3, uniqueQuestions(
+                    listOf(
+                        "a",
+                        "b",
+                        "c"
+                    )
                 )
             )
-        )
-    }
+        }
 
-    @Test
-    fun `Example 4`() {
-        assertEquals(
-            3, uniqueQuestions(
-                listOf(
-                    "ab",
-                    "ac"
+        @Test
+        fun `Example 4`() {
+            assertEquals(
+                3, uniqueQuestions(
+                    listOf(
+                        "ab",
+                        "ac"
+                    )
                 )
             )
-        )
-    }
+        }
 
-    @Test
-    fun `Example 5`() {
-        assertEquals(
-            1, uniqueQuestions(
-                listOf(
-                    "a",
-                    "a",
-                    "a",
-                    "a"
+        @Test
+        fun `Example 5`() {
+            assertEquals(
+                1, uniqueQuestions(
+                    listOf(
+                        "a",
+                        "a",
+                        "a",
+                        "a"
+                    )
                 )
             )
-        )
-    }
+        }
 
-    @Test
-    fun `Example 6`() {
-        assertEquals(
-            1, uniqueQuestions(
-                listOf(
-                    "b"
+        @Test
+        fun `Example 6`() {
+            assertEquals(
+                1, uniqueQuestions(
+                    listOf(
+                        "b"
+                    )
                 )
             )
-        )
+        }
+
+        @Test
+        fun `Total Unique Questions`() {
+            val input = listOf(
+                "abc",
+                "",
+                "a",
+                "b",
+                "c",
+                "",
+                "ab",
+                "ac",
+                "",
+                "a",
+                "a",
+                "a",
+                "a",
+                "",
+                "b"
+            )
+            assertEquals(11, totalUniqueQuestions(ParseUtil.parseGroups(input)))
+        }
     }
 
-    @Test
-    fun `Total Unique Questions`() {
-        val input = listOf(
-            "abc",
-            "",
-            "a",
-            "b",
-            "c",
-            "",
-            "ab",
-            "ac",
-            "",
-            "a",
-            "a",
-            "a",
-            "a",
-            "",
-            "b"
-        )
-        assertEquals(11, totalUniqueQuestions(input))
+    @Nested
+    inner class AllAnsweredYes {
+        @Test
+        fun `Example 1`() {
+            assertEquals(
+                3, allAnsweredYes(
+                    listOf(
+                        "abcx",
+                        "abcy",
+                        "abcz"
+                    )
+                )
+            )
+        }
+
+        @Test
+        fun `Example 2`() {
+            assertEquals(
+                3, allAnsweredYes(
+                    listOf(
+                        "abc"
+                    )
+                )
+            )
+        }
+
+        @Test
+        fun `Example 3`() {
+            assertEquals(
+                0, allAnsweredYes(
+                    listOf(
+                        "a",
+                        "b",
+                        "c"
+                    )
+                )
+            )
+        }
+
+        @Test
+        fun `Example 4`() {
+            assertEquals(
+                1, allAnsweredYes(
+                    listOf(
+                        "ab",
+                        "ac"
+                    )
+                )
+            )
+        }
+
+        @Test
+        fun `Example 5`() {
+            assertEquals(
+                1, allAnsweredYes(
+                    listOf(
+                        "a",
+                        "a",
+                        "a",
+                        "a"
+                    )
+                )
+            )
+        }
+
+        @Test
+        fun `Example 6`() {
+            assertEquals(
+                1, allAnsweredYes(
+                    listOf(
+                        "b"
+                    )
+                )
+            )
+        }
+
+        @Test
+        fun `Total Unique Questions`() {
+            val input = listOf(
+                "abc",
+                "",
+                "a",
+                "b",
+                "c",
+                "",
+                "ab",
+                "ac",
+                "",
+                "a",
+                "a",
+                "a",
+                "a",
+                "",
+                "b"
+            )
+            assertEquals(6, totalAllAnswered(ParseUtil.parseGroups(input)))
+        }
+
     }
+
 }
