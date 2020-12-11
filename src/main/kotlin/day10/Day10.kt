ec16f511a2def7e7e1dae213903940b33ca27998
Eric Jean <eric.jean@wwt.com>
2020-12-11 14:26:05 -0700

    Day 10 Part 2

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -10,11 +10,42 @@ fun differencesWhenConnected(joltsOfAdapters: List<Int>): Map<Int, Int> {
     return (1..3).map { differenceValue ->
         differenceValue to differences.count { it == differenceValue }
     }.toMap()
-
 }
+
+fun possibleCombinations(joltsOfAdapters: List<Int>) : Long {
+    val max = joltsOfAdapters.maxOrNull()!!
+    val joltsSorted = (joltsOfAdapters + 0 + (max + 3)).sorted()
+    val joltsSet = joltsSorted.toSet()
+    val children = joltsSorted.fold(emptyMap<Int, Set<Int>>()) { mapSoFar, joltValue ->
+        mapSoFar + (joltValue to childrenOf(joltValue, joltsSet))
+    }
+    val combosThroughAdaptor = joltsSorted.foldRight(emptyMap<Int, Long>()) { joltValue, combosThroughAdaptorSoFar ->
+        children[joltValue]?.let { childrenForAdaptor ->
+            val combosForThisAdaptor = if (childrenForAdaptor.isEmpty()) {
+                1L
+            } else {
+                childrenForAdaptor.fold(0L) { sum, child ->
+                    sum + combosThroughAdaptorSoFar[child]!!
+                }
+            }
+//            println("F[$joltValue] = $combosForThisAdaptor")
+            combosThroughAdaptorSoFar + (joltValue to combosForThisAdaptor)
+        } ?: throw IllegalStateException("No children value found")
+    }
+    return combosThroughAdaptor[0]!!
+}
+
+private fun childrenOf(joltValue: Int, joltsOfAdapters: Set<Int>) : Set<Int> =
+    (1..3).fold(emptySet()) { childrenSoFar, diff ->
+        val possibleChild = joltValue + diff
+        if (joltsOfAdapters.contains(possibleChild)) childrenSoFar + possibleChild else childrenSoFar
+    }
+
+
 
 fun main() {
     val joltsOfAdapters = ParseUtil.inputLines(10).map { it.toInt() }
     val differences = differencesWhenConnected(joltsOfAdapters)
     println("Part 1 = ${differences[1]!! * differences[3]!!}")
+    println("Part 2 = ${possibleCombinations(joltsOfAdapters)}")
 }
--- src/test/kotlin/day10/Day10Test.kt
+++ src/test/kotlin/day10/Day10Test.kt
@@ -5,66 +5,40 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class Day10Test {
+    private val smallInput = listOf(16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4)
+    val largeInput = listOf(
+        28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49,
+        45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3
+    )
+
     @Nested
     inner class DifferencesWhenConnected {
         @Test
         fun `small example`() {
-            val input = listOf(
-                16,
-                10,
-                15,
-                5,
-                1,
-                11,
-                7,
-                19,
-                6,
-                12,
-                4
-            )
-            val result = differencesWhenConnected(input)
+            val result = differencesWhenConnected(smallInput)
             assertEquals(7, result[1])
             assertEquals(5, result[3])
         }
 
         @Test
         fun `large example`() {
-            val input = listOf(
-                28,
-                33,
-                18,
-                42,
-                31,
-                14,
-                46,
-                20,
-                48,
-                47,
-                24,
-                23,
-                49,
-                45,
-                19,
-                38,
-                39,
-                11,
-                1,
-                32,
-                25,
-                35,
-                8,
-                17,
-                7,
-                9,
-                4,
-                2,
-                34,
-                10,
-                3
-            )
-            val result = differencesWhenConnected(input)
+
+            val result = differencesWhenConnected(largeInput)
             assertEquals(22, result[1])
             assertEquals(10, result[3])
+        }
+    }
+
+    @Nested
+    inner class PossibleCombinations {
+        @Test
+        fun `small example`() {
+            assertEquals(8, possibleCombinations(smallInput))
+        }
+
+        @Test
+        fun `large example`() {
+            assertEquals(19208, possibleCombinations(largeInput))
         }
     }
 }
