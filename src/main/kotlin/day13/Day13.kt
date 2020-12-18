295283a86963ce8417bab7df7a7315002c3950e8
Eric Jean <eric.jean@wwt.com>
2020-12-18 02:34:33 -0700

    Day 13 Part 2

--- src/main/kotlin/day13/Day13.kt
+++ src/main/kotlin/day13/Day13.kt
@@ -3,18 +3,60 @@ package day13
 import util.ParseUtil
 import java.lang.RuntimeException
 
-fun parseBusIds(input: String) : Set<Int> =
+fun parseBusIds(input: String): List<String> =
     input.split(",")
-        .filterNot { it == "x" }
-        .map { it.toInt() }
-        .toSet()
 
-fun busToCatch(time: Int, busIds: Set<Int>) : Pair<Int, Int> =
-    busIds.map { busId ->
-        busId to (busId * (time/busId + 1)) - time
-    }.minByOrNull {
-        it.second
-    } ?: throw RuntimeException("Unable to find min")
+fun busToCatch(time: Int, busIds: List<String>): Pair<Int, Int> =
+    busIds.filterNot { it == "x" }
+        .map { it.toInt() }
+        .map { busTime ->
+            busTime to (busTime * (time / busTime + 1)) - time
+        }.minByOrNull {
+            it.second
+        } ?: throw RuntimeException("Unable to find min")
+
+fun timeMatchingOffsets(busIds: List<String>): Long {
+    val idsWithOffset = busIds.mapIndexed { index, id -> id to index }
+        .filterNot { it.first == "x" }
+        .map { it.first.toLong() to it.second }
+//    var n = idsWithOffset.first().first.toLong()
+//    while(n < 1000) {
+//        val matches = idsWithOffset.filter {
+//            val valueToTry = n + it.second
+//            (valueToTry % it.first) == 0.toLong()
+//        }.map { it.first }
+//        if (matches.size > 1) {
+//            println("$n matches $matches")
+//        }
+//        n += idsWithOffset.first().first
+//    }
+
+    val initial = idsWithOffset.first().first
+    return idsWithOffset.subList(1, idsWithOffset.size).fold(StepInfo(initial, initial)) { acc, i ->
+        firstMatch(acc.value, i.first, i.second, acc.step)
+    }.value
+}
+
+data class StepInfo(
+    val value: Long,
+    val step: Long
+)
+
+fun firstMatch(base: Long, value: Long, offset: Int, stepSize: Long) : StepInfo {
+    var n = base
+    while((n + offset) % value != 0.toLong()) {
+        n += stepSize
+    }
+    println("Found match $base $value $offset at $n")
+    val firstIntersection = n
+    n += stepSize
+    while((n + offset) % value != 0.toLong()) {
+        n += stepSize
+    }
+    println("Found next match $base $value $offset at $n")
+    val secondMatch = n
+    return StepInfo(firstIntersection, secondMatch - firstIntersection)
+}
 
 fun main() {
     val input = ParseUtil.inputLines(13)
@@ -23,4 +65,6 @@ fun main() {
 
     val earliestBus = busToCatch(time, busIds)
     println("Part 1 = ${earliestBus.first * earliestBus.second}")
+
+    println("Part 2 = ${timeMatchingOffsets(busIds)}")
 }
--- src/test/kotlin/day13/Day13Test.kt
+++ src/test/kotlin/day13/Day13Test.kt
@@ -1,12 +1,54 @@
 package day13
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class Day13Test {
+    private val busIds = parseBusIds("7,13,x,x,59,x,31,19")
+
     @Test
     fun `Earliest Bus Example`() {
-        val busIds = parseBusIds("7,13,x,x,59,x,31,19")
         assertEquals(Pair(59, 5), busToCatch(939, busIds))
     }
+
+    @Nested
+    inner class TimeMatchingOffsets {
+        @Test
+        fun `First example`() {
+            assertEquals(1068781, timeMatchingOffsets(busIds))
+        }
+
+        @Test
+        fun `simple example`() {
+            //println("${firstMatch(7, 13, 1)}")
+            assertEquals(77, timeMatchingOffsets(parseBusIds("7,13")))
+        }
+
+        @Test
+        fun `The earliest timestamp that matches the list 17,x,13,19 is 3417`() {
+            assertEquals(3417, timeMatchingOffsets(parseBusIds("17,x,13,19")))
+        }
+
+        @Test
+        fun `67,7,59,61 first occurs at timestamp 754018`() {
+            assertEquals(754018, timeMatchingOffsets(parseBusIds("67,7,59,61")))
+        }
+
+        @Test
+        fun `67,x,7,59,61 first occurs at timestamp 779210`() {
+            assertEquals(779210, timeMatchingOffsets(parseBusIds("67,x,7,59,61")))
+        }
+
+        @Test
+        fun `67,7,x,59,61 first occurs at timestamp 1261476`() {
+            assertEquals(1261476, timeMatchingOffsets(parseBusIds("67,7,x,59,61")))
+        }
+
+        @Test
+        fun `1789,37,47,1889 first occurs at timestamp 1202161486`() {
+            assertEquals(1202161486, timeMatchingOffsets(parseBusIds("1789,37,47,1889")))
+        }
+    }
+
 }
