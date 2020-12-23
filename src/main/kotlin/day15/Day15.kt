d765812351dfd8eb8e1c807a4fd067f104bfc259
Eric Jean <eric.jean@wwt.com>
2020-12-23 16:46:48 -0700

    Day 15 Part 2

--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -1,33 +1,55 @@
 package day15
 
 data class GameState(
-    val spoken: Int,
-    val turnNumber: Int,
-    val turnLastSpoken: Map<Int, Int>
+    val spoken: Long,
+    val turnNumber: Long,
+    val turnLastSpoken: Map<Long, Long>
 )
 
-fun playGame(startingNumbers: List<Int>): Int =
-    numberSpoken(initializeGameState(startingNumbers), 2020)
+fun playGame(startingNumbers: List<Long>, turnToStop: Long): Long =
+    numberSpoken(initializeGameState(startingNumbers), turnToStop)
 
-private tailrec fun numberSpoken(gameState: GameState, turnToStop: Int): Int =
+private tailrec fun numberSpoken(gameState: GameState, turnToStop: Long): Long =
     if (gameState.turnNumber == turnToStop) {
         gameState.spoken
     } else {
         numberSpoken(takeTurn(gameState), turnToStop)
     }
 
-private fun initializeGameState(startingNumbers: List<Int>): GameState {
+private fun initializeGameState(startingNumbers: List<Long>): GameState {
     val turnLastSpoken = (0 until startingNumbers.size - 1).map { index ->
-        startingNumbers[index] to index + 1
+        startingNumbers[index] to index.toLong() + 1
     }.toMap()
     return GameState(
         spoken = startingNumbers.last(),
-        turnNumber = startingNumbers.size,
+        turnNumber = startingNumbers.size.toLong(),
         turnLastSpoken = turnLastSpoken
     )
 }
 
+fun playGameMutable(startingNumbers: List<Long>, turnToStop: Long): Long {
+    val turnLastSpoken = (0 until startingNumbers.size - 1).map { index ->
+        startingNumbers[index] to index.toLong() + 1
+    }.toMap().toMutableMap()
+
+    var spokenLastTurn = startingNumbers.last()
+    var turnNumber = startingNumbers.size.toLong() + 1
+
+    while (turnNumber <= turnToStop) {
+        val lastTurnNumber = turnNumber - 1
+        val valueToSpeak = turnLastSpoken[spokenLastTurn]?.let { lastTurnNumber - it } ?: 0
+        turnLastSpoken[spokenLastTurn] = lastTurnNumber
+
+        spokenLastTurn = valueToSpeak
+        turnNumber++
+    }
+    return spokenLastTurn
+}
+
 private fun takeTurn(gameState: GameState): GameState {
+    if (gameState.turnNumber % 10000 == 0.toLong()) {
+        println("Turn=${gameState.turnNumber} Value=${gameState.spoken}")
+    }
     val valueToSpeak = gameState.turnLastSpoken[gameState.spoken]?.let { gameState.turnNumber - it } ?: 0
     return GameState(
         spoken = valueToSpeak,
@@ -37,7 +59,8 @@ private fun takeTurn(gameState: GameState): GameState {
 }
 
 fun main() {
-    val input = listOf(2, 20, 0, 4, 1, 17)
+    val input = listOf(2, 20, 0, 4, 1, 17).map { it.toLong() }
 
-    println("Part 1 = ${playGame(input)}")
+    println("Part 1 = ${playGame(input, 2020)}")
+    println("Part 2 = ${playGameMutable(input, 30000000)}")
 }
--- src/test/kotlin/day15/Day15Test.kt
+++ src/test/kotlin/day15/Day15Test.kt
@@ -6,40 +6,54 @@ import org.junit.jupiter.api.Test
 
 class Day15Test {
     @Nested
-    inner class PlayGame {
+    inner class PlayShortGame {
         @Test
         fun `Given the starting numbers 0,3,6, the 2020th number spoken is 436`() {
-            assertEquals(436, playGame(listOf(0, 3, 6)))
+            assertEquals(436, playGameMutable(listOf(0, 3, 6), 2020))
         }
 
         @Test
         fun `Given the starting numbers 1,3,2, the 2020th number spoken is 1`() {
-            assertEquals(1, playGame(listOf(1, 3, 2)))
+            assertEquals(1, playGame(listOf(1, 3, 2), 2020))
         }
 
         @Test
         fun `Given the starting numbers 2,1,3, the 2020th number spoken is 10`() {
-            assertEquals(10, playGame(listOf(2, 1, 3)))
+            assertEquals(10, playGame(listOf(2, 1, 3), 2020))
         }
 
         @Test
         fun `Given the starting numbers 1,2,3, the 2020th number spoken is 27`() {
-            assertEquals(27, playGame(listOf(1, 2, 3)))
+            assertEquals(27, playGame(listOf(1, 2, 3), 2020))
         }
 
         @Test
         fun `Given the starting numbers 2,3,1, the 2020th number spoken is 78`() {
-            assertEquals(78, playGame(listOf(2, 3, 1)))
+            assertEquals(78, playGame(listOf(2, 3, 1), 2020))
         }
 
         @Test
         fun `Given the starting numbers 3,2,1, the 2020th number spoken is 438`() {
-            assertEquals(438, playGame(listOf(3, 2, 1)))
+            assertEquals(438, playGame(listOf(3, 2, 1), 2020))
         }
 
         @Test
         fun `Given the starting numbers 3,1,2, the 2020th number spoken is 1836`() {
-            assertEquals(1836, playGame(listOf(3, 1, 2)))
+            assertEquals(1836, playGame(listOf(3, 1, 2), 2020))
         }
+    }
+
+    @Nested
+    inner class PlayLongGame {
+        @Test
+        fun `Given 0,3,6, the 30000000th number spoken is 175594`() {
+            assertEquals(175594, playGameMutable(listOf(0, 3, 6), 30000000))
+        }
+
+        @Test
+        fun `Given 1,3,2, the 30000000th number spoken is 2578`() {
+            assertEquals(2578, playGameMutable(listOf(1,3,2), 30000000))
+        }
+
     }
 }
